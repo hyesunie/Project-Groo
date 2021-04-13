a4baa84df59402fb586b223c33c4be014bfebad2
hyesunie <hyesunie1025@gmail.com>
2021-04-13 20:52:13 +0900

    feat: basic structure of css file

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import routes from "./routes";
 
 const app = express();
 
-app.set("views", __dirname + "/view");
+app.use("/assets", express.static("assets"));
+app.use("/node_modules", express.static("node_modules"));
+
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
+app.set("views", __dirname + "/view");
 
 app.get(routes.home, (req, res) => {
   try {
