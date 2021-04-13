3a872850329f4668d649ce976fe64bdf2a99b6be
hyesunie <hyesunie1025@gmail.com>
2021-04-13 14:00:40 +0900

    Create server

--- init.js
+++ init.js
@@ -2,10 +2,6 @@ import app from "./app";
 
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`listening port ${port}`);
 });
