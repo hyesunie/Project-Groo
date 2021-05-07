bd5b4a996ffb49837cf187530c264dc1c3038773
hyesunie <hyesunie1025@gmail.com>
2021-05-07 17:17:52 +0900

    feat: use webpack

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 import "./partials/sidebar.js";
 import "./partials/userConfirmation.js";
 import "./partials/loadStorageInfo.js";
+import "./partials/controlHabits.js";
+import "./partials/controlComment.js";
+
+import "../css/style.css";
